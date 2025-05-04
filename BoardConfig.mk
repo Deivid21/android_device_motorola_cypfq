139c35854c4f4b4afdb8fd8e5207f241a1e2eda5
marcost2 <marcostolcachir@gmail.com>
2025-05-03 23:42:00 -0300

    Revert "fogos: disable brightness slider gamma conversion"
    This reverts commit 2c2bd4c5dd87da1595280adbb477b8c94eee5f89.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ TARGET_TAP_TO_WAKE_NODE := /sys/class/touchscreen/primary/gesture
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
